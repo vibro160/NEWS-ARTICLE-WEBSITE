1bc5cb77ce1d099511ab9480a4869634d4f7d7d7
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-08-05 22:23:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ searchinput.addEventListener("keypress",function (e){
 });
 
 
-fun
+function fetchNews(query){
+    const apikey='e2b2fe87b91f441ab0e1df7c45e1c2c2';
+    const Query = document.getElementById("searchInput").value.trim();
+   const url = `https://newsapi.org/v2/everything?q=${Query}&apiKey=${apikey}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const articles = data.articles;
+      displayNews(articles);
+    })
+    .catch(error => {
+      console.error('Error fetching news:', error);
+    });
+
+}
